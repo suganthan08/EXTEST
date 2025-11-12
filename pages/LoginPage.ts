e359d1202437ed52e050e59be163cbde1352202d
suganthan08 <sk2606251@gmail.com>
2025-11-12 14:49:49 +0530

    new one

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -11,5 +11,8 @@ export class Loginpage{
         await this.usernameField.fill(username);
         await this.passwordField.fill(password);
         await this.loginButton.click();
+
+
+        
     }
 }
