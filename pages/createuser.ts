bd1960fea1ba95e80595df1ea47410e7b238e15b
suganthan08 <sk2606251@gmail.com>
2025-12-13 17:37:25 +0530

    verify my code and debug it

--- pages/createuser.ts
+++ pages/createuser.ts
@@ -2,21 +2,21 @@ import { StatusCodes } from "./statuscodes";
 import { randomUser } from "./utils/library";
 import { expect } from "@playwright/test";
 
-// export class APIClient {
-//   constructor(private request: any) {}
+export class APIClient {
+  constructor(private request: any) {}
 
-//   async createUser() {
-//     const user = randomUser();
-//     const res = await this.request.post(`${BASE_URL}${API.USERS}`, { data: user });
+  async createUser() {
+    const user = randomUser();
+    const res = await this.request.post(`${BASE_URL}${API.USERS}`, { data: user });
 
-//     if (res.status() === StatusCodes.BAD_REQUEST) {
-//       console.error("❌ Endpoint limit reached — get a new API key from crudcrud.com");
-//       throw new Error("CrudCrud limit exceeded");
-//     }
+    if (res.status() === StatusCodes.BAD_REQUEST) {
+      console.error("❌ Endpoint limit reached — get a new API key from crudcrud.com");
+      throw new Error("CrudCrud limit exceeded");
+    }
 
-//     expect(res.status()).toBe(StatusCodes.CREATED);
-//     const body = await res.json();
-//     console.log("🟢 Created:", body);
-//     return body;
-//   }
-// }
+    expect(res.status()).toBe(StatusCodes.CREATED);
+    const body = await res.json();
+    console.log("🟢 Created:", body);
+    return body;
+  }
+}
